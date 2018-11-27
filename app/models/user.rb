a490a836295dd20bb3ca9577f3cf2096027ace38
SamarAhmeed <samarabdo1811@gmail.com>
2018-11-27 08:30:56 +0200

    User Table Done

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates_uniqueness_of :phone_number
+  validates :full_name, presence: true, length: {minimum: 6}, on: :create, allow_nil: false
+end
